e06ce3f754334c9b86da57ac5b4bf753a9217afe
unknown <vnd22victor@gmail.com>
2024-04-23 15:48:20 -0300

    portfolio

--- admin/secciones/portafolio/index.php
+++ admin/secciones/portafolio/index.php
@@ -1,3 +1,19 @@
 <?php include("../../templates/header.php");?>
-Listar portafolio
+
+<div class="card">
+    <div class="card-header">
+        <a
+            name=""
+            id=""
+            class="btn btn-primary"
+            href="#"
+            role="button"
+            >Agregar registro</a
+        >  
+    </div>
+
+    
+</div>
+
+
 <?php include("../../templates/footer.php");?>
