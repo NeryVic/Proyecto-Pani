bd48e77408e85700de81cca0c498bb1ff9239b62
unknown <vnd22victor@gmail.com>
2024-04-22 21:13:14 -0300

    kk

--- index.php
+++ index.php
@@ -65,21 +65,22 @@ $lista_servicios=$sentencia->fetchAll(PDO::FETCH_ASSOC);
         <section class="page-section" id="services">
             <div class="container">
                 <div class="text-center">
-                
                     <h2 class="section-heading text-uppercase">productos destacados</h2>
                     <h3 class="section-subheading text-muted"></h3>
                 </div>
+                
                     <div class="row text-center">
                     <?php foreach($lista_servicios as $registros){ ?>
                     <div class="col-md-4">
                         <span class="fa-stack fa-4x">
                             <i class="fas fa-circle fa-stack-2x text-primary"></i>
-                            <i class="fas <?php echo $registros["icono"];?>fa-stack-1x fa-inverse"></i>
+                            <i class="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
                         </span>
-                        <h4 class="my-3"><?php echo $registros["Titulo"];?></h4>
+                        <h4 class="my-3">E-Commerce</h4>
                         <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
                     </div>
-                    <?php } ?>
+                    <?php }?>
+
                     <div class="col-md-4">
                         <span class="fa-stack fa-4x">
                             <i class="fas fa-circle fa-stack-2x text-primary"></i>
@@ -99,18 +100,14 @@ $lista_servicios=$sentencia->fetchAll(PDO::FETCH_ASSOC);
                 </div>
             </div>
                 <!-- Carousel -->
-                
                 <div id="carouselExampleCaptions" class="carousel slide">
-                
                     <div class="carousel-indicators">
                       <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                       <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
                       <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
                     </div>
-                   
                     <div class="carousel-inner">
                       <div class="carousel-item active">
-                        
                         <img src="assets/img/portfolio/1.jpg" class="d-block w-100" alt="...">
                         <div class="carousel-caption d-none d-md-block">
                           <h5>Primera Imagen</h5>
