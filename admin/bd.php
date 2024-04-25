a7b8525b89f06266d9f492b4c254b9d8d25974eb
ALEJANDRO PINTOS. <148916132+NOVATCODER@users.noreply.github.com>
2024-04-25 13:30:14 -0300

    Aparentemente recupere conexion con la bd

--- admin/bd.php
+++ admin/bd.php
@@ -8,7 +8,7 @@ $contrasenia="";
 try{
 
     $conexion=new PDO("mysql:host=$servidor;dbname=$BaseDeDatos", $usuario, $contrasenia);
-    echo ("Conexión realizada...");}
+    echo ("Conexión realizada...!");}
 catch(Exception $error){
     echo $error ->getMessage();
 }
