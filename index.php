299b62adbbfa401f5bd3d8b50c06cea8346d9a3d
unknown <vnd22victor@gmail.com>
2024-04-24 22:16:35 -0300

    a

--- index.php
+++ index.php
@@ -81,46 +81,8 @@ $lista_servicios=$sentencia->fetchAll(PDO::FETCH_ASSOC);
 
                 </div>
             </div> 
-                <!-- Carousel -->
-                <!-- 
-                <div id="carouselExampleCaptions" class="carousel slide">
                 
-                    <div class="carousel-indicators">
-                      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                    </div>
-                    
-                    <div class="carousel-inner">
-                    
-                      <div class="carousel-item active">
 
-                      <?php foreach($lista_servicios as $registros){ ?>
-                        <img src="assets/img/servicios/1.jpg" class="d-block w-100" alt="...">
-                        <div class="carousel-caption d-none d-md-block">
-                          <h5><?php echo $registros['titulo'] ?></h5>
-                          <p><?php echo $registros['descripcion'] ?></p>
-                        </div>
-                        <?php }?>
-                       
-                      </div>
-                     
-                      <div class="carousel-item">
-                        <img src="assets/img/portfolio/2.jpg" class="d-block w-100" alt="...">
-                        <div class="carousel-caption d-none d-md-block">
-                          <h5>Segunda Imagen</h5>
-                          <p>Some representative placeholder content for the second slide.</p>
-                        </div>
-                      </div>
-                      <div class="carousel-item">
-                        <img src="assets/img/portfolio/4.jpg" class="d-block w-100" alt="...">
-                        <div class="carousel-caption d-none d-md-block">
-                          <h5>Tercera</h5>
-                          <p>Some representative placeholder content for the third slide.</p>
-                        </div>
-                      </div>
-                    </div>
-                            -->
                     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
                       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                       <span class="visually-hidden">Previous</span>
