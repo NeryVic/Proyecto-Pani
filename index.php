b9828a5ee2f2f9309434c019d272ae019f367605
unknown <vnd22victor@gmail.com>
2024-05-05 17:30:03 -0300

    contacto listo

--- index.php
+++ index.php
@@ -322,9 +322,6 @@ $lista_servicios=$sentencia->fetchAll(PDO::FETCH_ASSOC);
             </div>
         </section>
         <!-- Clients-->
-
-
-
         <!-- Contact-->
         <section class="page-section" id="contact">
             <div class="container">
@@ -332,13 +329,6 @@ $lista_servicios=$sentencia->fetchAll(PDO::FETCH_ASSOC);
                     <h2 class="section-heading text-uppercase">CONTÁCTENOS</h2>
                     <h3 class="section-subheading text-muted">Su consulta nos interesa</h3>
                 </div>
-                <!-- * * * * * * * * * * * * * * *-->
-                <!-- * * SB Forms Contact Form * *-->
-                <!-- * * * * * * * * * * * * * * *-->
-                <!-- This form is pre-integrated with SB Forms.-->
-                <!-- To make this form functional, sign up at-->
-                <!-- https://startbootstrap.com/solution/contact-forms-->
-                <!-- to get an API token!-->
                 <form id="contactForm">
                     <div class="row align-items-stretch mb-5">
                         <div class="col-md-6">
@@ -351,12 +341,10 @@ $lista_servicios=$sentencia->fetchAll(PDO::FETCH_ASSOC);
                                 <!-- Email address input-->
                                 <input class="form-control" id="email_id" name="email_id" type="email" placeholder="Tu Email *" data-sb-validations="required,email" />
                                 <div class="invalid-feedback" data-sb-feedback="email:required">An email is required.</div>
-                                <div class="invalid-feedback" data-sb-feedback="email:email">Email is not valid.</div>
                             </div>
                             <div class="form-group mb-md-0">
                                 <!-- Phone number input-->
                                 <input class="form-control" id="phone" name="phone" type="tel" placeholder="Tu teléfono (opcional)" data-sb-validations="required" />
-                                <div class="invalid-feedback" data-sb-feedback="phone:required">A phone number is required.</div>
                             </div>
                             
                         </div>
@@ -368,32 +356,12 @@ $lista_servicios=$sentencia->fetchAll(PDO::FETCH_ASSOC);
                             </div>
                         </div>
                     </div>
-                    <!-- Submit success message-->
-                    <!---->
-                    <!-- This is what your users will see when the form-->
-                    <!-- has successfully submitted-->
-                    <div class="d-none" id="submitSuccessMessage">
-                        <div class="text-center text-white mb-3">
-                            <div class="fw-bolder">Form submission successful!</div>
-                            To activate this form, sign up at
-                            <br />
-                            <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
-                        </div>
-                    </div>
-                    <!-- Submit error message-->
-                    <!---->
-                    <!-- This is what your users will see when there is-->
-                    <!-- an error submitting the form-->
-                    <div class="d-none" id="submitErrorMessage"><div class="text-center text-danger mb-3">Error sending message!</div></div>
                     <!-- Submit Button-->
                     <div class="text-center">          
                         <input class="btn btn-primary btn-xl text-uppercase " id="button" type="submit" value="Enviar">
                     </div>
-                    
                 </form>
             </div>
-
-
         </section>
         <!-- Footer-->
         <footer class="footer py-4">
--- js/contact.js
+++ js/contact.js
@@ -1,20 +1,50 @@
-//
-
 const btn = document.getElementById('button');
+const form = document.getElementById('contactForm');
+const nameInput = document.getElementById('from_name');
+const emailInput = document.getElementById('email_id');
+const phoneInput = document.getElementById('phone');
+const messageInput = document.getElementById('message');
 
-document.getElementById('contactForm')
- .addEventListener('submit', function(event) {
-   event.preventDefault();
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
 
-   btn.value = 'enviando...';
+  // Validación del nombre
+  const name = nameInput.value;
+  if (name === '') {
+    swal('Por favor, ingresa tu nombre.');
+    return;
+  }
 
-   const serviceID = 'default_service';
-   const templateID = 'template_v4sj38e';
+  // Validación del email
+  const email = emailInput.value;
+  if (email === '') {
+    swal('Por favor, ingresa tu correo electrónico.');
+    return;
+  }
 
-   emailjs.sendForm(serviceID, templateID, this)
+  // Validación del mensaje
+  const message = messageInput.value;
+  if (message === '' || message.trim() === '' || message.length <= 20) {
+    swal('Por favor, ingresa un mensaje mayor a 20 caracteres.');
+    return;
+  }
+
+  // Si todos los campos requeridos están completos, proceder con el envío
+  btn.value = 'enviando...';
+
+  const serviceID = 'default_service';
+  const templateID = 'template_v4sj38e';
+
+  emailjs.sendForm(serviceID, templateID, this)
     .then(() => {
       btn.value = 'enviar';
       swal('Gracias por contactarnos. En breve nos pondremos en contacto contigo.', '', 'success');
+
+      // Limpia los campos
+      nameInput.value = '';
+      emailInput.value = '';
+      messageInput.value = '';
+      phoneInput.value = '';
     }, (err) => {
       btn.value = 'enviar';
       alert(JSON.stringify(err));
