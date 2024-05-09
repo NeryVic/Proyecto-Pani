fcfcebec8a72b16abd84615c5a97f4dbbe0a9c2b
unknown <vnd22victor@gmail.com>
2024-05-09 17:00:06 -0300

    index port

--- admin/secciones/portafolio/index.php
+++ admin/secciones/portafolio/index.php
@@ -31,62 +31,8 @@ include("../../templates/header.php");
 
 <div class="card">
     <div class="card-header"></div>
-    <a href="crear.php" class="btn btn-primary" role="button">Agregar registro</a>
+    <a href="crear.php" class="btn btn-primary" role="button">Agregar registroogit</a>
     <div class="card-body">
-<<<<<<< HEAD
-
-    <div
-        class="table-responsive-sm"
-    >
-        <table
-            class="table"
-        >
-            <thead>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Titulo</th>
-                    <th scope="col">Subtitulo</th>
-                    <th scope="col">Imagen</th>
-                    <th scope="col">Descripcion</th>
-                    <th scope="col">Acciones</th>
-                
-                
-                
-                
-                </tr>
-            </thead>
-            <tbody>
-            <?php foreach($lista_portafoilio as $registros){ ?>
-                    
-                
-                    <tr class="">
-                <td scope="col"><?php echo $registros['ID']; ?></td>
-                    <td scope="col"><?php echo $registros['titulo']; ?></td>
-                    <td scope="col"><?php echo $registros['subtitulo']; ?></td>
-                    <td scope="col" <?php echo $registros['imagen']; ?> ></td>
-                    <td scope="col"><?php echo $registros['descripcion']; ?></td>
-                    <td scope="col"><a
-                            name=""
-                            id=""
-                            class="btn btn-info"
-                            href="editar.php?txtID=<?php echo $registros['ID'];?>"
-                            role="button"
-                            >Editar</a
-                        >
-                        
-                        <a
-                            name=""
-                            id=""
-                            class="btn btn-danger"
-                            href="index.php?txtID=<?php echo $registros['ID'];?>"
-                            role="button"
-                            >Eliminar</a
-                        >
-                    </td>
-                </tr>
-            
-            <?php }?>
-=======
         <div class="table-responsive-sm">
             <table class="table">
                 <thead>
@@ -113,7 +59,6 @@ include("../../templates/header.php");
                         </td>
                     </tr>
                 <?php } ?>
->>>>>>> 4e183b1c23b81cf40b462254a1f90b454452b8c1
                 </tbody>
             </table>
         </div>
