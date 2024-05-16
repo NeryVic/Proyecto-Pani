2fcda102eb8b503a729ccc80c6fcd8fbb29b4e9d
Nery <vnd22victor@gmail.com>
2024-05-15 21:50:51 -0300

    ahora si

--- admin/secciones/equipo/crear.php
+++ admin/secciones/equipo/crear.php
@@ -15,8 +15,9 @@ if ($_POST){
     move_uploaded_file($tmp_imagen, "../../../assets/img/team/".$nombre_archivo_imagen2); 
     }
 
-    $sentencia=$conexion->prepare("INSERT INTO tbl_equipo 'ID', 'imagen', 'nombrecompleto', 'puesto', 'twitter', 'facebook', 'linkedin')
-    VALUES (NULL,:imagen, :nombrecompleto, :puesto, :twitter, :facebook, :linkedin);");
+    $sentencia = $conexion->prepare("INSERT INTO tbl_equipo (imagen, nombrecompleto, puesto, twitter, facebook, linkedin) 
+    VALUES (:imagen, :nombrecompleto, :puesto, :twitter, :facebook, :linkedin)");
+
 
     $sentencia->bindParam(":imagen",$nombre_archivo_imagen2);
     $sentencia->bindParam(":nombrecompleto",$nombrecompleto);
