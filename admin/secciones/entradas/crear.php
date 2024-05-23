50123073078f430ebe5e7e0a1cc1ae8f52737008
ALEJANDRO PINTOS. <148916132+NOVATCODER@users.noreply.github.com>
2024-05-22 21:32:07 -0300

    update

--- admin/secciones/entradas/crear.php
+++ admin/secciones/entradas/crear.php
@@ -1,3 +1,37 @@
 <?php include("../../templates/header.php");?>
 Crear entrada
+
+
+<div class="card">
+    <div class="card-header">Crear entradas</div>
+    <div class="card-body">
+    <div
+        class="table-responsive-sm"
+    >
+        <table
+            class="table"
+        >
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Fecha</th>
+                    <th scope="col">Título</th>
+                    <th scope="col">Descrcipción</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="">
+                    <td scope="row">R1C1</td>
+                    <td>R1C2</td>
+                    <td>R1C3</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    
+    
+    </div>
+    <div class="card-footer text-muted">Footer</div>
+</div>
+
 <?php include("../../templates/footer.php");?>
